8e3285969a039c07effc40ba2221c4d77775a826
Raghu K <raghuk.vit@gmail.com>
2017-11-17 02:39:30 +0000

    code

---
+++ ansiblecode/playbooks/files/student.sql
@@ -0,0 +1,11 @@
+use studentapp;
+CREATE TABLE if not exists Students(student_id INT NOT NULL AUTO_INCREMENT,
+	student_name VARCHAR(100) NOT NULL,
+    student_addr VARCHAR(100) NOT NULL,
+	student_age VARCHAR(3) NOT NULL,
+	student_qual VARCHAR(20) NOT NULL,
+	student_percent VARCHAR(10) NOT NULL,
+	student_year_passed VARCHAR(10) NOT NULL,
+	PRIMARY KEY (student_id)
+);
+grant all privileges on studentapp.* to 'student'@'10.128.0.5' identified by 'student@1';
