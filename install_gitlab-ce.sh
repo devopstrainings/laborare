b7acba3ad874a1b11cddbf9f9189259d7042f09e
carreerit <rwn0517@gmail.com>
2017-11-27 07:20:07 +0530

    Create install_gitlab-ce.sh

---
+++ install_gitlab-ce.sh
@@ -0,0 +1,8 @@
+sudo yum install postfix
+sudo systemctl enable postfix
+sudo systemctl start postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+
+echo "Run this command by replacing gitlab.example.com with External / Public IP address"
+echo 'sudo EXTERNAL_URL="http://gitlab.example.com" yum install -y gitlab-ee'
