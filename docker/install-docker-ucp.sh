9d1560b50f3553467d746024f32c7c7c473b7ee8
carreerit <rwn0517@gmail.com>
2017-12-12 07:53:11 +0530

    Create install-docker-ucp.sh

---
+++ docker/install-docker-ucp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\n\e[31m Installing Docker\e[0m"
+curl -s https://raw.githubusercontent.com/linuxautomations/docker/master/install-ce.sh | bash
+
+echo -e "\nInstalling UCP\n"
+
+#yum install bind-utils -y &>/dev/null
+
+IP=$(host `hostname` | awk '{print $NF}')
+docker image pull docker/ucp:2.2.4
+docker container run --rm -it --name ucp \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  docker/ucp:2.2.4 install \
+  --host-address $IP \
+  --admin-username admin \
+  --admin-password password 
